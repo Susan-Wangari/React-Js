80c1dec4597c8890929be84847962657e703492b
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-11-08 18:05:21 +0300

    stateless functional components

---
+++ statelessComponent.js
@@ -0,0 +1,11 @@
+const MyComponent = function() {
+  // Change code below this line
+return (
+  <div>
+  <p>Welcome to React class.</p>
+  </div>
+);
+}
+
+//functions in React have to start with a capital letter
+//stateless functions are made using JavaScript
