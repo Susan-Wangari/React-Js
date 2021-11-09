b3497ca5d12d4f0a2ec9f2b3e68edfb409f0e944
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-11-09 12:16:52 +0300

    react components

---
+++ ReactComponent.js
@@ -0,0 +1,18 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // Change code below this line
+  return (
+    <div>
+    <h1>Hello React!</h1>
+    </div>
+  )
+
+
+    // super() to call the constructor of the parent class, in this case React.Component
+  }
+};
+//The constructor is a special method used during the initialization of objects that are created with the class keyword
+//define a React component with the ES6 class syntax.
