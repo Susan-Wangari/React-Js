89b223c037a238ddf4d741bf19e981f8e91369c3
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-11-11 19:28:07 +0300

    JavaScript code in JSX

--- javaScriptInJSX.js
+++ javaScriptInJSX.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 // Write code here:
 ReactDOM.render(
   //js in jsx
-  <h1>2 + 3</h1>, 
+  <h1>{2 + 3}</h1>, 
   document.getElementById('app')
 );
+//for code to be interpreted as js code, use curly braces to enclose it
