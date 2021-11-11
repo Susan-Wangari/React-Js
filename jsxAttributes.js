3722e946d07b7e428ff46775f52008e0b09aea5c
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-11-11 16:59:47 +0300

    creating attributes using JSX

---
+++ jsxAttributes.js
@@ -0,0 +1,4 @@
+const p1 = <p id="large">foo</p>;
+const p2 = <p id="small">bar</p>;
+//A JSX attribute is written using HTML-like syntax
+//a name, followed by an equals sign, followed by a value. 
