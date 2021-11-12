e93d77038679a705477fd783bf419cf0b26f7190
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-11-12 15:35:42 +0300

    ternary operators in jsx

---
+++ ternaryOperator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+//ternary operator
+const img = <img src={pics[coinToss() === 'heads' ? 'kitty' :'doggy']} />;
+
+ReactDOM.render(
+	img, 
+	document.getElementById('app')
+);
+//you write x ? y : z, where x, y, and z are all JavaScript expressions. When your code is executed, x is evaluated as either “truthy” or “falsy.” If x is truthy, then the entire ternary operator returns y. If x is falsy, then the entire ternary operator returns z
