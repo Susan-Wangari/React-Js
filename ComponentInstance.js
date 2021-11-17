9ef4d6ba82aedbfdcfde6f6cc109b26b5de027ed
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-11-17 16:43:04 +0300

    creating a component instance

---
+++ ComponentInstance.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+// component goes here:
+<MyComponentClass />
+//starts with a capital letter to differentiate it from HTML-like jsx
