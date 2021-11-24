d90323bfa5ce2a0abc152a35aa75685ec53148ea
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-11-24 21:16:21 +0300

    passing props to a component

---
+++ passProps.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render goes here:
+//the prop name and value
+ReactDOM.render(<PropsDisplayer myProp="Hello" />, document.getElementById('app'));
+//passing information to a React component
+//giving a component an attribute
